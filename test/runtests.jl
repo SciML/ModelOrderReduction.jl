b27977f58dddab11fb8e1f3cc0f98d139c3cd15c
Rahul Manavalan <rahul.manavalan@tum.de>
2022-06-30 15:17:47 +0200

    Add initial test
    Added prelim test for the reduction of the Lorenz time series.
    I know apriori that the energy of the system after reduction is >0.9.
    A more systematic test setup has to be thought up.
    Adding visualization of the data overlayed with the reduced basis would
    be nice!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,36 @@
 using ModelOrderReduction
 using Test
+using OrdinaryDiffEq
 
-@testset "ModelOrderReduction.jl" begin
 
+#--------- Data Reduction -----------------#
+@testset "PCA" begin
+    function lorenz_prob()
+        function lorenz!(du,u,p,t)
+            du[1] = p[1]*(u[2]-u[1])
+            du[2] = u[1]*(p[2]-u[3]) - u[2]
+            du[3] = u[1]*u[2] - p[3]*u[3]
+        end
+
+        u0 = [1,0,0]
+        p = [10,28,8/3]
+        tspan = (0,100)
+        prob = ODEProblem(lorenz!,u0,tspan,p)
+        sol = solve(prob,Tsit5())
+        sol
+    end
+
+    sol = lorenz_prob()
+    solution = Matrix(reduce(hcat,sol.u)')
+
+    order = 2
+    reducer = PCA(solution,order)
+    reduce!(reducer)
+
+    # Ad-hoc tests. To be checked with Chris.
+    @test length(reducer.rbasis) == reducer.nmodes
+    @test length(reducer.rbasis[1]) == size(solution,2)
+    @test reducer.energy > 0.9
 end
+
+#---------- Model Reduction ----------------#
