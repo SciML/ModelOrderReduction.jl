2fdf35869a583943571bd0082723de9b59502824
Rahul Manavalan <rahul.manavalan@tum.de>
2022-07-01 11:40:33 +0200

    Add solver types

--- src/Types.jl
+++ src/Types.jl
@@ -1,3 +1,8 @@
-abstract type AbstractReductionProblem end
+ abstract type AbstractReductionProblem end
 abstract type AbstractMORProblem <: AbstractReductionProblem end
 abstract type AbstractDRProblem <: AbstractReductionProblem end
+
+abstract type AbstractSVD end
+struct SVD <: AbstractSVD end
+struct TSVD <: AbstractSVD end
+struct RSVD <: AbstractSVD end
