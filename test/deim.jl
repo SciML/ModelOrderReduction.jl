fc89fa5eebd25cd6f2b5a588d66a51d478905762
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-07-16 14:49:42 -0700

    Update DEIM test

--- test/deim.jl
+++ test/deim.jl
@@ -1,10 +1,10 @@
 using Test
 using ModelOrderReduction
-using SparseArrays: sprand
+using SparseArrays
 using ModelingToolkit
-using LinearAlgebra: svd
+using LinearAlgebra
 
-const N = 10
+const N = 10 # number of dependent variables
 @variables t u[1:N](t)
 A = sprand(N, N, 0.3)
 D = Differential(t)
@@ -13,6 +13,13 @@ f(x) = sin(x)
 const n_snapshot = 2N
 const pod_dim = 5
 pod_basis = @view svd(rand(N, n_snapshot)).U[:, 1:pod_dim]
-deim_sys = deim(sys, pod_basis)
-@test length(states(deim_sys)) == pod_dim
-@test length(equations(deim_sys)) == pod_dim
+sys_deim = deim(sys, pod_basis)
+@test length(states(sys_deim)) == pod_dim
+@test length(equations(sys_deim)) == pod_dim
+@test length(observed(sys_deim)) == length(observed(sys)) + N
+
+u0 = Dict(collect(u) .=> rand(N))
+tspan = (0.0, 1.0)
+prob = ODEProblem(sys, u0, tspan)
+prob_deim = ODEProblem(sys_deim, u0, tspan)
+@test pod_basis' * prob.u0 ≈ prob_deim.u0 # check projection for initial values
