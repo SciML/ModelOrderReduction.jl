684d14632bf924e7c2c01385f4c3d724f902b2af
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-08-22 15:08:31 -0700

    Implement function for extracting linear terms and don't expand

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,5 @@
+using OrderedCollections, SparseArrays
+
 """
 $(TYPEDSIGNATURES)
 
@@ -20,4 +22,77 @@ function get_deqs(sys::ODESystem)::Tuple{Vector{Equation}, Vector{Equation}}
         end
     end
     deqs, others
+end
+
+"""
+$(SIGNATURES)
+
+Given a vector of expressions `exprs` and variables `vars`,
+returns a sparse coefficient matrix `A`, constant terms `c` and nonlinear terms `n`,
+such that `exprs = A * vars + c + n`,
+where the constant terms do not contain any variables in `vars`.
+
+Varaibles in `vars` must be unique.
+"""
+function linear_terms(exprs::AbstractVector, vars)
+    vars = OrderedSet(Symbolics.unwrap.(vars))
+    exprs = Symbolics.unwrap.(exprs)
+    linear_I = Int[] # row idx for sparse matrix
+    linear_J = Int[] # col idx for sparse matrix
+    linear_V = Real[] # values
+    idxmap = Dict(v => i for (i, v) in enumerate(vars))
+
+    # (a function used to avoid code duplication)
+    # when a linear term is identified, add the indices and value to the sparse matrix
+    @inline function push_sparse_coeff(row_index, term, value)
+        push!(linear_I, row_index)
+        push!(linear_J, idxmap[term])
+        push!(linear_V, value)
+        nothing
+    end
+
+    const_terms = similar(exprs, Num) # create a vector of the same size
+    const_terms .= 0 # manually set to Int 0 because `Num` doesn't have a corresponding zero
+    nonlinear_terms = similar(exprs, Num)
+    nonlinear_terms .= 0
+
+    # check if the expr is a constant or nolinear term about vars
+    # and add it to the corresponding collection
+    @inline function const_nonlinear(i, expr)
+        # expr is nonlinear if it contains any variable in vars
+        if Symbolics.has_vars(expr, vars)
+            nonlinear_terms[i] += expr
+        else # expr is constant if it doesn't have vars
+            const_terms[i] += expr
+        end
+        nothing
+    end
+
+    for (i, expr) in enumerate(exprs)
+        if expr isa Number # just a number, e.g. Int, Float64
+            const_terms[i] = expr
+        elseif expr in vars # expr is a variables in vars
+            push_sparse_coeff(i, term, 1)
+        elseif expr isa SymbolicUtils.Mul && length(expr.dict) == 1
+            base, exp = first(expr.dict)
+            if base in vars && exp == 1 # a var with a coeff
+                push_sparse_coeff(i, term, expr.coeff)
+            else
+                const_nonlinear(i, expr)
+            end
+        elseif expr isa SymbolicUtils.Add
+            const_terms[i] += expr.coeff
+            for (term, coeff) in expr.dict
+                if term in vars
+                    push_sparse_coeff(i, term, coeff)
+                else
+                    const_nonlinear(i, term * coeff)
+                end
+            end
+        else
+            const_nonlinear(i, expr)
+        end
+    end
+    linear = sparse(linear_I, linear_J, linear_V, length(exprs), length(vars))
+    return linear, const_terms, nonlinear_terms
 end
