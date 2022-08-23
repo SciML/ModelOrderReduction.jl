887696f3f5bdd40abb580f85ea380724828bcaa8
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-08-23 13:11:10 -0700

    Comment tearing_substitution

--- src/deim.jl
+++ src/deim.jl
@@ -61,6 +61,8 @@ function deim(sys::ODESystem, pod_basis::AbstractMatrix;
               deim_basis::AbstractMatrix = pod_basis,
               deim_dim::Integer = size(pod_basis, 2),
               name::Symbol = Symbol(nameof(sys), "_deim"))::ODESystem
+    # handle ODESystem.substitutions
+    # https://github.com/SciML/ModelingToolkit.jl/issues/1754
     sys = tearing_substitution(sys)
 
     iv = ModelingToolkit.get_iv(sys) # the single independent variable
