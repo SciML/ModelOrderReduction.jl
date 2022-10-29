48cfe4ac0d20c729615b2b0c9c507cc63ec57793
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-10-29 16:36:00 -0400

    Split tests into groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using SafeTestsets
 
-@safetestset "POD" begin include("DataReduction.jl") end
-@safetestset "utils" begin include("utils.jl") end
-@safetestset "DEIM" begin include("deim.jl") end
-@safetestset "PCE" begin include("pce.jl") end
+const GROUP = get(ENV, "GROUP", "All")
+
+@time begin
+    if GROUP == "All" || GROUP == "DEIM"
+        @time @safetestset "POD" begin include("DataReduction.jl") end
+        @time @safetestset "utils" begin include("utils.jl") end
+        @time @safetestset "DEIM" begin include("deim.jl") end
+    end
+    if GROUP == "All" || GROUP == "PCE"
+        @time @safetestset "PCE" begin include("pce.jl") end
+    end
+end
