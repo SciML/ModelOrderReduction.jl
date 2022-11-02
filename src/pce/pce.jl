69bf14262648757e97b249bc7880c587dfd3d103
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-11-02 12:22:13 -0400

    Define `TensorProductOrthoPoly`

--- src/pce/pce.jl
+++ src/pce/pce.jl
@@ -1,3 +1,67 @@
+function grevlex(n::Int, grade::Int)
+    if n == 1
+        return reshape([grade], 1, 1)
+    end
+
+    if grade == 0
+        return zeros(Int, 1, n)
+    end
+
+    sub_ind = grevlex(n - 1, grade)
+    ind = hcat(sub_ind, zeros(Int, size(sub_ind, 1)))
+    for k in 1:grade
+        sub_ind = grevlex(n - 1, grade - k)
+        ind = vcat(ind, hcat(sub_ind, k * ones(Int, size(sub_ind, 1))))
+    end
+    return ind
+end
+function grevlex(n::Int, grades::AbstractVector{Int})
+    return reduce(vcat, [grevlex(n, grade) for grade in grades])
+end
+function grevlex(n::Int, grade::Int, max_degrees::Vector{Int})
+    return grevlex(n, grade, [0:d for d in max_degrees])
+end
+function grevlex(n::Int, grades::AbstractVector{Int}, max_degrees::Vector{Int})
+    return reduce(vcat, [grevlex(n, grade, max_degrees) for grade in grades])
+end
+function grevlex(n::Int, grade::Int, degree_constraints::Vector{<:AbstractVector})
+    if n == 1
+        return grade in degree_constraints[1] ? reshape([grade], 1, 1) : zeros(Int, 0, 1)
+    end
+    if grade == 0
+        return all(0 in degs for degs in degree_constraints) ? zeros(Int, 1, n) :
+               zeros(Int, 0, n)
+    end
+    filtered_grades = filter(x -> x <= grade, degree_constraints[end])
+    sub_ind = grevlex(n - 1, grade - filtered_grades[1], degree_constraints[1:(end - 1)])
+    ind = hcat(sub_ind, filtered_grades[1] * ones(Int, size(sub_ind, 1)))
+    for k in filtered_grades[2:end]
+        sub_ind = grevlex(n - 1, grade - k, degree_constraints[1:(end - 1)])
+        ind = vcat(ind, hcat(sub_ind, k * ones(Int, size(sub_ind, 1))))
+    end
+    return ind
+end
+function grevlex(n::Int, grades::AbstractVector{Int},
+                 degree_constraints::Vector{<:AbstractVector})
+    return reduce(vcat, [grevlex(n, grade, degree_constraints) for grade in grades])
+end
+
+struct TensorProductOrthoPoly <: AbstractOrthoPoly{ProductMeasure, EmptyQuad{Float64}}
+    deg::Vector{Int}
+    ind::Matrix{Int}
+    measure::ProductMeasure
+    uni::Vector
+end
+function TensorProductOrthoPoly(ops::AbstractVector{T}) where {T <: AbstractOrthoPoly}
+    n = length(ops)
+    degrees = deg.(ops)
+    ind = grevlex(n, 0:maximum(degrees), degrees)
+    measures = [op.measure for op in ops]
+    w(t) = prod(m.w(t) for m in measures)
+    measure = ProductMeasure(w, measures)
+    TensorProductOrthoPoly(degrees, ind, measure, ops)
+end
+
 """
 $(TYPEDEF)
 
--- test/pce.jl
+++ test/pce.jl
@@ -1,14 +1,21 @@
 using Test, ModelOrderReduction
 using Symbolics, PolyChaos
+import ModelOrderReduction as MOR
+
+supp = (-1, 1)
+w(t) = 1 + t
+my_meas = Measure("my_meas", w, supp, false, Dict())
+deg = 4
+my_op = OrthoPoly("my_op", deg, my_meas; Nquad = 200)
 
 @testset "PCE constructor" begin
     x = @variables a b c d
-    supp = (-1, 1)
-    w(t) = 1 + t
-    my_meas = Measure("my_meas", w, supp, false, Dict())
-    deg = 4
-    my_op = OrthoPoly("my_op", deg, my_meas; Nquad = 200)
     basis = [HermiteOrthoPoly(3), Uniform01OrthoPoly(4), Uniform_11OrthoPoly(5), my_op]
     ind = rand(Int, 5, 4)
     @test_nowarn PCE(x, basis, ind)
+end
+
+@testset "TensorProductOrthoPoly" begin
+    ops = [GaussOrthoPoly(4), Uniform01OrthoPoly(2), LaguerreOrthoPoly(3), my_op]
+    @test_nowarn MOR.TensorProductOrthoPoly(ops)
 end
