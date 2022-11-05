10e3bd37358c4c1d105fcc440b3d55971aa6d384
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-11-05 04:27:45 -0400

    Revert "Split tests into groups"
    This reverts commit 48cfe4ac0d20c729615b2b0c9c507cc63ec57793.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,6 @@
 using SafeTestsets
 
-const GROUP = get(ENV, "GROUP", "All")
-
-@time begin
-    if GROUP == "All" || GROUP == "DEIM"
-        @time @safetestset "POD" begin include("DataReduction.jl") end
-        @time @safetestset "utils" begin include("utils.jl") end
-        @time @safetestset "DEIM" begin include("deim.jl") end
-    end
-    if GROUP == "All" || GROUP == "PCE"
-        @time @safetestset "PCE" begin include("pce.jl") end
-    end
-end
+@time @safetestset "POD" begin include("DataReduction.jl") end
+@time @safetestset "utils" begin include("utils.jl") end
+@time @safetestset "DEIM" begin include("deim.jl") end
+@time @safetestset "PCE" begin include("pce.jl") end
